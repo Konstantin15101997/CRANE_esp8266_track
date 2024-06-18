15859eb822af0c9e972de8c349860c2f61f6f1a5
kukuikar <denis@perepetsky.ru>
2024-06-18 16:07:14 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -41,8 +41,7 @@ void loop() {
   motor1.tick();
   motor2.tick();
   motor3.tick();
-  int speed;
-  int mode;
+  int speed_mode[3];
   
   if (WiFi.status() == WL_CONNECTED) {
     // Прием данных по UDP
@@ -55,13 +54,28 @@ void loop() {
         GParser data(incomingPacket, ',');
         int am = data.split(); 
         //Serial.printf("%s\n",incomingPacket);
-        mode=data.getInt(0);
+
+        //v1.0
+        /*mode=data.getInt(0);
         speed=map(data.getInt(1),172,1811,-255,255);
-        speed = (speed>=-2 && speed<=2) ? 0 : speed;   //условие Тернарный оператор
-        Serial.println(speed);
+        speed = (speed>=-2 && speed<=2) ? 0 : speed;*/
+
+        //v1.1
+        for (int i=0;i<3;i++){
+          speed_mode[i]= (i==0) ? data.getInt(i): map(data.getInt(i),172,1811,-255,255);
+        }
+        speed_mode[1] = (speed_mode[1]>=-2 && speed_mode[1]<=2) ? 0 : speed_mode[1];
+        speed_mode[2] = (speed_mode[2]>=-2 && speed_mode[2]<=2) ? 0 : speed_mode[2];
+        for (int i=0;i<3;i++){
+          Serial.print(speed_mode[i]);
+          Serial.print(" ");
+        }
+        Serial.println();
       }
     }
-    if (mode==0){
+
+    //v1.0
+    /*if (mode==0){
       speed=0;
     }else if (mode==1){
       motor1.setSpeed(speed);
@@ -69,8 +83,38 @@ void loop() {
     }else if (mode==2){
       motor1.setSpeed(speed);
       motor2.setSpeed((-1)*speed);
+    }*/
+
+    //v1.1
+    int8_t k1; //коэффициенты моторов
+    int8_t k2;
+    if (speed_mode[0]==0){
+      speed_mode[1]=0;
+      speed_mode[2]=0;
+    }else{
+      if (speed_mode[1]>=0 && speed_mode[2]>=0){  
+        if (speed_mode[1]==0){
+          speed_mode[1]=speed_mode[2];
+          k1=k2=-1;
+        }else if (speed_mode[2]==0){
+          speed_mode[2]=speed_mode[1];
+          k1=1;
+          k2=-1;
+        }
+        speed_mode[2]=255-speed_mode[1];
+        k1=k2=1;
+      }else if(speed_mode[1]==0 && speed_mode[2]==0){
+        k1=k2=0;
+      }
     }
-  }else{speed=0;}
+
+    motor1.setSpeed((k1)*speed_mode[1]);
+    motor2.setSpeed((k2)*speed_mode[2]);
+
+  }else{
+    speed_mode[1]=0;
+    speed_mode[2]=0;
+  }
 
   delay(10);
 }
